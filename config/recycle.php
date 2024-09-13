3067dc5e9bd88e5c461d62198e4bc06dd32cd2d2
Ahtnezev <ahtnezev@gmail.com>
2024-09-13 10:17:33 -0600

    Deploy app

---
+++ config/recycle.php
@@ -0,0 +1,37 @@
+<?php
+/**------------------------------------------------------------------------
+ *?   Archivo para obtener textos que se van a reciclar en todo el sitio.
+ *------------------------------------------------------------------------**/
+
+return [
+    'metas' => [
+        'keywords' => 'Nana Mala KEYWORDS...',
+        'description' => 'Nana Mala DESCRIPTION...',
+        'theme_color' => '#F17F2F', // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meta/name/theme-color
+    ],
+    'otros' => [
+        'url' => [
+            'facebook' => 'https://www.google.com/',
+            'instagram' => 'https://www.google.com/',
+        ],
+    ],
+    'navbar' => [
+        'inicio' => 'INICIO',
+        'nosotros' => 'Nosotros',
+        'menu' => 'Nuestro Menú',
+        'ubicacion' => 'Ubicación',
+        'galeria' => 'Galería',
+    ],
+    'banner' => [
+        'title' => 'LOREM IPSUM',
+        'text' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A non hic placeat maxime est? Quam maxime qui labore necessitatibus.',
+        'alt' => 'Descripcion imagen'
+    ],
+    'footer' => [
+        'aviso_privacidad' => 'Aviso de privacidad',
+        'copy' => 'Copyright',
+        'credits' => [
+            'email' => 'mailto:Vicente<dev.ahtnezev@gmail.com>'
+        ]
+    ]
+];
---
+++ resources/views/partials/metas.blade.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Vicente Neri">
+    <meta name="theme-color" content="{{ config('recycle.metas.theme_color') }}" />
+    <meta name="description" content="{{ config('recycle.metas.description') }}">
+    <meta name="keywords" content="{{ config('recycle.metas.keywords') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/nana_mala.ico') }}">
+    <title>{{ env('APP_NAME', 'Nana Mala') }} | Guanatos</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
